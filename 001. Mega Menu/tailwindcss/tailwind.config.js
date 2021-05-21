77017da40faa23c937b3391a3d516908c89fc3cb
tamhoangweb <tamhoangweb@gmail.com>
2021-05-21 10:44:30 +0700

    Khởi tạo repo Tailwind, và cài đặt ban đầu của UI Mega menu

---
+++ 001. Mega Menu/tailwindcss/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Oswald", "sans-serif"],
+      },
+    },
+  },
+  variants: {
+    extend: {
+      display: ["group-hover"],
+      margin: ["group-hover"],
+      visibility: ["group-hover"],
+    },
+  },
+  plugins: [],
+};
